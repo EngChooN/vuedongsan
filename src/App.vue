c25e319d9c488f3f2358bf059004d8ad56e6ecb4
Joonyoung Cho <aimzero@vertigogames.com>
2022-07-28 18:41:29 +0900

    vuedongsanCreateProject

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(el, index) in menus" :key="index">{{ el }}</a>
+  </div>
+  <div v-for="(el, index) in products" :key="index">
+    <h4>{{ el.name }}</h4>
+    <p>{{ el.price }} 만원</p>
+    <button v-on:click="el.report++">허위매물신고</button>
+    <span>신고수: {{ el.report }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // 데이터 저장함 (변수선언)
+  data() {
+    return {
+      products: [
+        {
+          name: "역삼동원룸",
+          price: 60,
+          report: 0,
+        },
+        {
+          name: "천호동원룸",
+          price: 70,
+          report: 0,
+        },
+        {
+          name: "마포구원룸",
+          price: 100,
+          report: 0,
+        },
+      ],
+      menus: ["Home", "Products", "About"],
+    };
+  },
+  // 힘수저장함 (함수선언)
+  methods: {},
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +49,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
