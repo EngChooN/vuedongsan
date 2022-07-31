3be61f9c7d32b9ea52e22724cfd4aafd99d7def9
준영 <aimzero9303@gmail.com>
2022-07-31 21:03:58 +0900

    ()-practice

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,13 +2,6 @@
   <!-- 상품리스트 -->
   <div>
     <img v-bind:src="el.image" class="room-img" />
-    <!-- <h4
-      v-on:click="
-        modalOpen = true;
-        clickValue = index;
-      "
-      class="product-name"
-    > -->
     <h4 @click="$emit('openModal', el.id)" class="product-name">
       {{ el.title }}
     </h4>
